521ff304db34ba1c7852d7f063c7d3b922797ef0
nami sabet <nami.sabet@hotmail.com>
2019-10-01 18:27:43 -0400

    Add project files.

---
+++ Models/Sub.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StarSubsSheet5.Models
+{
+
+    public enum SubName
+    {
+        TheBrahms,
+        TheTchaikovsky,
+        TheMahler,
+        TheStrauss,
+        TheRachmaninoff
+    }
+    public enum Size
+    {
+        OneHitWonder,
+        BList,
+        AList,
+        SuperStar
+    }
+    public enum Meal
+    {
+        None,
+        DrinkChips,
+        DrinkCookies
+    }
+
+
+    public class Sub
+    {
+
+        public string MealNumber { get; set; }
+        public SubName SubName { get; set; }
+        public Size Size { get; set; }
+        public Meal Meal { get; set; }
+    }
+}
